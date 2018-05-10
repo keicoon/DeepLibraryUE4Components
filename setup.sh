3eb16720c1f8f0e909576e563214caf6b9285a49
keicoon15 <keicoon15@gmail.com>
2018-05-10 19:46:28 +0900

    add thirdparty - CNTK, Tensorflow

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+Version=1.0.0
+
+if [ -d "ThirdParty/CNTK/lib" ]; then
+    echo "ThirParty libraries is ready to downloaded"
+else
+    if [ -f "ThirdParty/$ZipFile" ]; then
+        echo "Okay you have zip"
+    else
+        echo "Download prebuilt ThirdParty libraries for DeepLibrary"
+        (cd ThirdParty; curl -O -L https://github.com/keicoon/DeepLibraryUE4Components/releases/download/$Version/libs.zip)
+    fi
+
+    (cd ThirdParty; unzip $ZipFile;)
+fi
